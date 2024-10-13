a53f3773235953147c7ffad9d64e67cf99c813bd
Thiagox <almadathiagoignacio@gmail.com>
2024-10-13 12:04:31 -0300

    Nada

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ import Minijuego
 
 if __name__ == "__main__":
     EcoBot.Ciclo_Juego()  # Llama a la función de EcoBot
-    #Minijuego.play_minijuego()  # Llama a la función del mini juego si es necesario
